00d77f57e9978921ac170e85abb5b887a4b47016
janaravi <janath1134@gmail.com>
2017-07-25 13:02:35 +0530

    funwork

---
+++ script.js
@@ -0,0 +1,78 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+    SC.stream('/tracks/76327437',function(sound){
+         $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/94431903',function(sound){
+         $('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/84237364',function(sound){
+         $('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/112010796',function(sound){
+         $('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/134565638',function(sound){
+         $('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/254362918',function(sound){
+         $('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+});
